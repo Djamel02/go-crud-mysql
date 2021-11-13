67f08b15c65d63c34fb7e58476711534dd6bf444
Djamel Benbouzid <dbouzidi2@gmail.com>
2021-11-13 12:27:13 +0100

    delete CORS

--- main.go
+++ main.go
@@ -10,48 +10,6 @@ import (
 	"github.com/gorilla/mux"
 )
 
-const (
-	options           string = "OPTIONS"
-	allow_origin      string = "Access-Control-Allow-Origin"
-	allow_methods     string = "Access-Control-Allow-Methods"
-	allow_headers     string = "Access-Control-Allow-Headers"
-	allow_credentials string = "Access-Control-Allow-Credentials"
-	expose_headers    string = "Access-Control-Expose-Headers"
-	credentials       string = "true"
-	origin            string = "Origin"
-	methods           string = "POST, GET, OPTIONS, PUT, DELETE, HEAD, PATCH"
-
-	// If you want to expose some other headers add it here
-	headers string = "Access-Control-Allow-Origin, Accept, Accept-Encoding, Authorization, Content-Length, Content-Type, X-CSRF-Token,ApiKey"
-)
-
-// Handler will allow cross-origin HTTP requests
-func CORS(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Set allow origin to match origin of our request or fall back to *
-		if o := r.Header.Get(origin); o != "" {
-			w.Header().Set(allow_origin, o)
-		} else {
-			w.Header().Set(allow_origin, "*")
-		}
-
-		// Set other headers
-		w.Header().Set(allow_headers, headers)
-		w.Header().Set(allow_methods, methods)
-		w.Header().Set(allow_credentials, credentials)
-		w.Header().Set(expose_headers, headers)
-
-		// If this was preflight options request let's write empty ok response and return
-		if r.Method == options {
-			w.WriteHeader(http.StatusOK)
-			w.Write(nil)
-			return
-		}
-
-		next.ServeHTTP(w, r)
-	})
-}
-
 func main() {
 	router := mux.NewRouter()
 	db, err := dbconfig.Connect()
@@ -59,6 +17,6 @@ func main() {
 		panic(err)
 	}
 	routehandler.HandleEmpRoutes(router, db)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", dbconfig.GetEnvironmentVars("PORT")), CORS(router)))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", dbconfig.GetEnvironmentVars("PORT")), router))
 
 }
