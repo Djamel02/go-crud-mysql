a4764c32ac2c6e3b9ecea45cfffab87f2dd59f50
Djamel Benbouzid <dbouzidi2@gmail.com>
2021-11-16 07:59:43 +0100

    make respons as global into controllers

---
+++ controller/responseHandler.go
@@ -0,0 +1,20 @@
+package controller
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+// respondwithJSON write json response format
+func respondwithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	response, _ := json.Marshal(payload)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
+}
+
+// respondwithError return error message
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	respondwithJSON(w, code, map[string]string{"message": msg})
+}
